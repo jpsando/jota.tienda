e76c27184cb5d4d57979729b44a5a1226eaeaba6
Juan Pablo Sandoval <pablo@estudiotobogan.com>
2021-08-16 23:26:46 -0300

    mismo anterior

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ app.set('view engine', 'ejs');
 app.use(cors({ origin: "*" }));
 app.use(express.static(path.join(__dirname, 'public')))
 
-// if(process.env.NODE_ENV === 'production'){
-//     const path  =  require('path');
-//     app.get('/*',(req,res)=>{
-//         res.sendfile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+if(process.env.NODE_ENV === 'production'){
+    const path  =  require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 //Separamos los array de dataSite en los hijos
 let products = dataSite[0].products,
